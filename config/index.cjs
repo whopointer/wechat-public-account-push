7854f35a6d017184ede0e281f15a5461e4bf5a7b
冀云龙 <88369435+whopointer@users.noreply.github.com>
2023-04-27 19:44:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,25 +7,28 @@
  */
 const USER_CONFIG = {
 
+  // 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'push-plus',
+  
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江苏',
+  CITY: '南通',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: 'bubble小姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ed29358c82244ba69fe1f9bf15170b48',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '09-22',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
