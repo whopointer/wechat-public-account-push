a5f74150127f7c64535056a98dba304101e76449
冀云龙 <88369435+whopointer@users.noreply.github.com>
2024-06-16 23:37:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -28,7 +28,7 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'lQAGxmxxd4IRoSUfyf37KN05T2kxe12ovu0V2xX1Rm8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '09-22',
+      horoscopeDate: '09-05',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
