4edc7ee1acba0756bfa1b127180c3b6225b4cfae
冀云龙 <88369435+whopointer@users.noreply.github.com>
2024-06-16 21:18:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,7 +26,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKooJ6ZkWT9VjK3dIHx-l-NVCg-I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'OoExDxHuREYRgik2zIdxSKJ_RtcfYeOxWXohXyIUNpQ',
+      useTemplateId: 'dX4tSJs9_zpY-Cs4y4zXWLH5cReVl7bE2MLtHf877J8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-22',
       festivals: [
